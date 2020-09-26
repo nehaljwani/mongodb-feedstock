8467122cca7bfae83105602085ffafb61900a91e
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-09-26 18:40:14 -0400

    Enable builds for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -26,4 +26,4 @@ set "_scons_xtra_flags=%_scons_xtra_flags% CXXFLAGS=/I%LIBRARY_INC%"
 set "_scons_xtra_flags=%_scons_xtra_flags% LINKFLAGS=/LIBPATH:%LIBRARY_LIB%"
 set "_scons_xtra_flags=%_scons_xtra_flags% CPPDEFINES=BOOST_ALL_DYN_LINK"
 
-%PYTHON% buildscripts/scons.py install core %_scons_xtra_flags%
+python buildscripts/scons.py install core %_scons_xtra_flags%
