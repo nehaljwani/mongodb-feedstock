7edb728370cf1aee3c502cb0782a82f65c3983c7
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-09-26 19:23:46 -0400

    Bump version to v4.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,17 +7,15 @@ set "_scons_xtra_flags=%_scons_xtra_flags% --disable-warnings-as-errors"
 set "_scons_xtra_flags=%_scons_xtra_flags% --enable-free-mon=on"
 set "_scons_xtra_flags=%_scons_xtra_flags% --enable-http-client=on"
 set "_scons_xtra_flags=%_scons_xtra_flags% --link-model=object"
-set "_scons_xtra_flags=%_scons_xtra_flags% --mmapv1=on"
-set "_scons_xtra_flags=%_scons_xtra_flags% --mobile-se=on"
 set "_scons_xtra_flags=%_scons_xtra_flags% --opt=on"
-set "_scons_xtra_flags=%_scons_xtra_flags% --prefix=%LIBRARY_PREFIX%"
 set "_scons_xtra_flags=%_scons_xtra_flags% --release"
 set "_scons_xtra_flags=%_scons_xtra_flags% --server-js=on"
-set "_scons_xtra_flags=%_scons_xtra_flags% --ssl=on --ssl-provider=openssl"
+set "_scons_xtra_flags=%_scons_xtra_flags% --ssl=on"
 set "_scons_xtra_flags=%_scons_xtra_flags% --wiredtiger=on"
 set "_scons_xtra_flags=%_scons_xtra_flags% VERBOSE=on"
+set "_scons_xtra_flags=%_scons_xtra_flags% DESTDIR=%LIBRARY_PREFIX%"
 
-for %%v in (boost icu pcre sqlite snappy yaml zlib zstd abseil-cpp) do (
+for %%v in (boost icu pcre snappy yaml zlib zstd abseil-cpp) do (
  set "_scons_xtra_flags=!_scons_xtra_flags! --use-system-%%v"
 )
 
@@ -26,4 +24,4 @@ set "_scons_xtra_flags=%_scons_xtra_flags% CXXFLAGS=/I%LIBRARY_INC%"
 set "_scons_xtra_flags=%_scons_xtra_flags% LINKFLAGS=/LIBPATH:%LIBRARY_LIB%"
 set "_scons_xtra_flags=%_scons_xtra_flags% CPPDEFINES=BOOST_ALL_DYN_LINK"
 
-python buildscripts/scons.py install core %_scons_xtra_flags%
+python buildscripts/scons.py install-core %_scons_xtra_flags%
